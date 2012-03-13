1b2adac847a060dae927654c8b2d2106f53379b0
Tomohito Ozaki <ozaki@yuroyoro.com>
2012-03-13 18:00:44 +0900

    added README.

---
+++ README.markdown
@@ -0,0 +1,11 @@
+Rubyz
+===============================================
+
+Scalazがよくわからんので
+
+  一人Scalaz Advent Calendar
+  http://partake.in/events/4b3afdc8-e4ec-4010-b8ec-31b89210dda0
+
+をRubyで写経してみる誰得企画
+
+なんとなく、Rubyで関数型っぽいのを。
--- spec/identity_spec.rb
+++ spec/identity_spec.rb
@@ -1,12 +1,5 @@
 # -*- coding: utf-8 -*-
 
-# Scalazがよくわからんので
-#
-#  一人Scalaz Advent Calendar
-#  http://partake.in/events/4b3afdc8-e4ec-4010-b8ec-31b89210dda0
-#
-# をRubyで写経してみる誰得企画
-#
 # No.1 Identity
 #   http://basking-cat.blogspot.com/2011/12/identity.html
 require 'spec_helper'
